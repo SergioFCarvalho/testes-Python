5ac5bed9bb1ddbdd660598435b05f74ac5781c2e
Sérgio Carvalho <sergiocarvalhotec@gmail.com>
2023-09-05 01:07:58 +0000

    rota e HTML da pagina princiapl

--- ocean/app.py
+++ ocean/app.py
@@ -1,7 +1,22 @@
 from flask import Flask
+from flask import render_template
 
-app = Flask("meu app")
+app = Flask("Meu App")
+
+posts = [
+    {
+        "titulo":"Primeira post",
+        "texto": "teste"
+    },
+    {
+        "titulo": "Segundo post",
+        "texto": "teste2"
+    },
+    
+]
 
 @app.route('/')
-def hello():
-    return "Hello World!!!"
+def exibir_entradas():
+    entradas = posts
+    return render_template('exibir_entradas.html', entradas=entradas)
+
