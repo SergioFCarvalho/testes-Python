a4aca91acbd2933bdf129e4b1b3798b1ed31e1ff
Sérgio Carvalho <sergiocarvalhotec@gmail.com>
2023-09-07 00:07:42 +0000

    login

--- ocean/app.py
+++ ocean/app.py
@@ -1,22 +1,31 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, request, session, flash, redirect, url_for
 
-app = Flask("Meu App")
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'pudim'
 
 posts = [
     {
-        "titulo":"Primeira post",
+        "titulo": "Minha primeira postagem",
         "texto": "teste"
     },
     {
-        "titulo": "Segundo post",
-        "texto": "teste2"
-    },
-    
+        "titulo": "Segundo Post",
+        'texto': "outro teste"
+    }
 ]
 
 @app.route('/')
 def exibir_entradas():
-    entradas = posts
+    entradas = posts # Mock das postagens
     return render_template('exibir_entradas.html', entradas=entradas)
 
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    erro = None
+    if request.method == "POST":
+        if request.form['username'] == "admin" and request.form['password'] == "admin":
+            session['logado'] = True
+            flash("Login efetuado com sucesso!")
+            return redirect(url_for('exibir_entradas'))
+        erro = "Usuário ou senha inválidos"        
+    return render_template('login.html', erro=erro)
